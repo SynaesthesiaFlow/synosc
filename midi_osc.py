35ce29e9e6829064d4c83ddef406e9b75ce09675
Davis Dulin <davisdulin@MacBook-Pro-2.local>
2019-08-02 03:38:04 -0700

    @.@

---
+++ midi_osc.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+# this example requires the pyliblo library from http://das.nasophon.de/pyliblo
+import liblo, sys
+
+# set the target address where MidiOSC will be listening (assumes the default port is used)
+try:
+    midiosc = liblo.Address("localhost", 8000)
+except liblo.AddressError as err:
+    print(err)
+    sys.exit()
+
+# change the device name to something appropriate for your system
+device_name = "IAC Driver Bus 1".replace(' ', '_')
+osc_address = "/midi/" + device_name + "/0"
+
+# send a note on with note number 60 and velocity 100
+liblo.send(midiosc, osc_address, "note_on", 60, 100)
+
+# send a note off with note number 60 and velocity 0
+liblo.send(midiosc, osc_address, "note_off", 60, 0)
+
+# now to receive some messages
+try:
+    server = liblo.Server(7001)
+except liblo.ServerError as err:
+    print(err)
+    sys.exit()
+
+def callback(path, args, types, src):
+    print("Got {0}".format(path))
+    for a, t in zip(args, types):
+        print("Argument type: {0}, value: {1}".format(t, a))
+
+server.add_method(None, None, callback)
+
+print("Kill the process with ctrl-c when you get bored")
+while True:
+    server.recv(100)
